441c1989203b70ce297647f1cf838183cde7a5c4
kailous <kailous@live.cn>
2023-08-24 02:18:53 +0800

    优化代码

--- README.md
+++ README.md
@@ -9,11 +9,9 @@
 - 在前端展示追番数据，包括番剧标题、集数、封面等。
 - 支持直接点击跳转到哔哩哔哩番剧页面。
 - 显示追番进度，包括已看集数、总集数、观看进度条。
-- 支持在 Vercel 上一键部署。
 
 ## 部署说明
-你可以直接点击下方按钮，一键部署到 Vercel 上。</br>
-
+复制本仓库到你的 Github 仓库，然后在 Vercel 上部署。</br>
 部署完成后，你需要配置环境变量</br>
 环境变量的字段名需要和下表一致，值则是你的 Bilibili Cookie 中对应的值。</br>
 
@@ -28,6 +26,4 @@
 |REACT_APP_bili_jct| bili_jct          |
 
 你可以在 Vercel 绑定自己的域名就可以访问了</br>
-
-[![Deploy to Vercel](https://vercel.com/button)](https://vercel.com/import/project?template=https://github.com/kailous/bilibili-anime-tracker)
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 // pages/index.tsx
 // Bilibili 番剧列表
 
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import Head from 'next/head';
 import styles from './AnimeList.module.css';
 import 'src/app/globals.css';
 
+// 创建Index组件
 function Index() {
+
+    // 定义Anime接口
     interface Anime {
         title: string;
         epNum: string;
@@ -19,13 +22,15 @@ function Index() {
         epEnd: number;
     }
 
+    //使用useState钩子来管理状态
     const [animeData, setAnimeData] = useState<Anime[]>([]);
 
+    // 使用 useEffect 钩子来在组件渲染时执行数据获取操作
     useEffect(() => {
-        fetch('/api/bilibili')
-            .then(response => response.json())
+        fetch('/api/bilibili') // 使用 fetch API 来获取数据
+            .then(response => response.json()) // 获取到数据后将其转换为 JSON 格式
             .then(data => {
-                setAnimeData(data);
+                setAnimeData(data); // 将获取到的数据设置到状态中
             })
             .catch(error => console.error('Error fetching data:', error));
     }, []);
@@ -33,26 +38,28 @@ function Index() {
     return (
         <div>
             <Head>
-                <meta name="referrer" content="never" />
+                <meta name="referrer" content="never"/>
             </Head>
             <ul className={styles['anime-list']}>
                 {animeData.map((anime, index) => (
                     <li className={styles['anime-item']} key={index}>
-                        <a className={styles['anime-link']} href={anime.epUrl} target="_blank" rel="noopener noreferrer">
-                            <img className={styles['anime-cover']} src={anime.epCover} alt={`Cover for ${anime.title}`} />
+
+                        {/*// 为每个番剧创建一个链接*/}
+                        <a className={styles['anime-link']} href={anime.epUrl} target="_blank"
+                           rel="noopener noreferrer" title={`${anime.title}\n第${anime.epStart}话-${anime.epTitle}`}>
+                            <img className={styles['anime-cover']} src={anime.epCover}
+                                 alt={`Cover for ${anime.title}`}/>
                             <div className={styles['info']}>
                                 <h2 className={styles['anime-title']}>{anime.title}</h2>
                                 <div className={styles['anime-episode']}>
-                                    <p>
-                                        {anime.epNum}/
-                                        {anime.epStart < anime.epEnd ? (
-                                            <span className={styles['watched-episode']}>
-                                        看到{anime.epStart}话{anime.epTime}
-                                    </span>
-                                        ) : (
-                                            `看到${anime.epStart}话`
-                                        )}
-                                    </p>
+
+                                    {/*// 看完的和没看完的显示不同的内容*/}
+                                    {anime.epStart < anime.epEnd ? (
+                                        <p>{anime.epNum}/<span className={styles['watched-episode']}>看到{anime.epStart}话{anime.epTime}</span></p>
+                                    ) : (
+                                        <p>已看完全部{anime.epStart}话</p>
+                                    )}
+                                    {/*鼠标指针滑过的时候显示完整的标题*/}
                                     <p>{anime.epTitle}</p>
                                     {/* 添加进度条 */}
                                     <div className={styles['progress-bar']}>
